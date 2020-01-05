20b157175646960c2dc421dbbe49ccf6ce1187e6
JimCannon <cannon@DESKTOP-I3G874K.localdomain>
2020-01-05 14:29:25 +0100

    routes not working properly

--- src/client/client.js
+++ src/client/client.js
@@ -2,11 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux' //provider communicate data from the store to any connected components in our app
 import Routes from './Routes';
+import reducers from './reducers';
+
+//a new redux store to use on the client side of our app.
+const store = createStore(reducers, {}, applyMiddleware(thunk)) // <-- read up on this.
 
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>, 
+  <Provider store={store}> // Provider gets store as props, provider listens to store.
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 );
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {renderToString } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Routes from '../client/Routes';
 
 
-export default (req) => {
+export default (req, store) => {
   const content = renderToString(
-    <StaticRouter location={req.path} context={{}}>
-      <Routes />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
   );
 
-  return (`
+  return `
     <html>
       <head></head>
       <body>
@@ -19,5 +22,5 @@ export default (req) => {
         <script src="bundle.js"></script>
       </body>
     </html>
-  `);
+  `;
 }
